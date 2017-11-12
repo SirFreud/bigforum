06ab8d1e655f82f2de140910e661deae4aaf5a2f
Sirfreud <mikevaldez92@gmail.com>
2017-11-12 11:09:31 -0600

    Fixed exception handling nightmare, wrote test for creating new threads

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,4 @@
-<?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+ <?php
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,6 +7,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/threads', 'ThreadsController@index');
+Route::resource('/threads', 'ThreadsController');
 Route::get('/threads/{thread}', 'ThreadsController@show');
 Route::post('/threads/{thread}/replies', 'RepliesController@store')->name('add_replies');
--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -9,13 +9,41 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class CreateThreadsTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testExample()
+
+    use DatabaseMigrations;
+
+    /** @test */
+    function guests_may_not_create_threads()
     {
-        $this->assertTrue(true);
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $thread = make('App\Thread');
+        $this->post('/threads', $thread->toArray());
     }
+    
+
+
+    /** @test */
+    function guests_cannot_see_the_create_thread_page()
+    {
+        $this->withExceptionHandling()
+            ->get('/threads/create')
+            ->assertRedirect('/login');
+    }
+    
+
+    /** @test */
+    function an_authenticated_user_can_create_new_forum_threads()
+    {
+        // Given we have a signed in user
+        $this->signIn();
+        // When we hit the endpoint to create a new thread
+        $thread = make('App\Thread');
+        $this->post('/threads', $thread->toArray());
+        // Then, when we visit the thread page 
+        $this->get($thread->path())
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+    }
+    
 }
