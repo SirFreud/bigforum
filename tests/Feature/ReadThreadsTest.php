9a61e42b6dfd3fb26167e0ec61d8f25daa0d70b2
Sirfreud <mikevaldez92@gmail.com>
2017-11-11 21:12:56 -0600

    Wrote tests for adding replies and included the addReply method in RepliesController

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -26,7 +26,7 @@ class ReadThreadsTest extends TestCase
     /** @test */
     public function a_user_can_read_a_single_thread()
     {
-        $this->get('/threads/' . $this->thread->id)
+        $this->get($this->thread->path())
             ->assertSee($this->thread->title);
     }
 
@@ -37,7 +37,7 @@ class ReadThreadsTest extends TestCase
             ->create(['thread_id' => $this->thread-id]);
 
         // When we visit a thread page we should see the replies
-        $this->get('/threads/' . $this->thread->id)
+        $this->get($this->thread->path())
             ->assertSee($reply->body);
     }
 }
